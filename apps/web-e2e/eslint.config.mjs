93d95c0c860485cfa1520251aa712d6963d26a8c
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-10-21 10:35:25 -0700

    chore(eslint): update eslint configuration in web-e2e

--- apps/web-e2e/eslint.config.mjs
+++ apps/web-e2e/eslint.config.mjs
@@ -2,11 +2,14 @@ import playwright from 'eslint-plugin-playwright';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
-  playwright.configs['flat/recommended'],
   ...baseConfig,
   {
     files: ['**/*.ts', '**/*.js'],
-    // Override or add rules here
-    rules: {},
+    plugins: {
+      playwright,
+    },
+    rules: {
+      ...playwright.configs.recommended.rules,
+    },
   },
 ];
