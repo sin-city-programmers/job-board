e78c566c8985c826f23ab232bb71cf8f8e5bbc8f
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-10-22 21:17:54 -0700

    chore(web): ensure web project using correct settings

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,5 @@
 //@ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
@@ -10,6 +9,10 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  // Use webpack instead of turbopack for better Nx compatibility
+  webpack: (config, { isServer }) => {
+    return config;
+  },
 };
 
 const plugins = [
